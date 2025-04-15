d4077cf54f38cc7125db2424cdd3d3c1a4cccb33
Viktoria Osmanova <victoria152331@gmail.com>
2025-04-15 23:39:59 +0300

    global heap

--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -4,24 +4,50 @@
 allocator_global_heap::allocator_global_heap(
     logger *logger)
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(logger *)", "your code should be here...");
+
+    if (_logger)
+        _logger->trace("allocator_global_heap::allocator_global_heap - begin");
+
+    _logger = logger;
+
+    if (_logger)
+        _logger->trace("allocator_global_heap::allocator_global_heap - end");
 }
 
 [[nodiscard]] void *allocator_global_heap::do_allocate_sm(
     size_t size)
 {
-    throw not_implemented("[[nodiscard]] void *allocator_global_heap::do_allocate_sm(size_t)", "your code should be here...");
+    if (_logger)
+        _logger->debug("allocator_global_heap::do_allocate_sm - begin");
+
+    try {
+        void *ptr = ::operator new(size);
+        if (_logger)
+            _logger->debug("allocator_global_heap::do_allocate_sm - end");
+        return ptr;
+    }
+    catch (const std::bad_alloc &) {
+        if (_logger)
+            _logger->error("allocator_global_heap::do_allocate_sm - std::bad_alloc thrown");
+        throw;
+    }
 }
 
 void allocator_global_heap::do_deallocate_sm(
     void *at)
 {
-    throw not_implemented("void allocator_global_heap::do_deallocate_sm(void *)", "your code should be here...");
+    if (_logger)
+        _logger->debug("allocator_global_heap::do_deallocate_sm - begin");
+
+    ::operator delete(at);
+
+    if (_logger)
+        _logger->debug("allocator_global_heap::do_deallocate_sm - end");
 }
 
 inline logger *allocator_global_heap::get_logger() const
 {
-    throw not_implemented("inline logger *allocator_global_heap::get_logger() const", "your code should be here...");
+    return _logger;
 }
 
 inline std::string allocator_global_heap::get_typename() const
@@ -31,30 +57,75 @@ inline std::string allocator_global_heap::get_typename() const
 
 allocator_global_heap::~allocator_global_heap()
 {
-    throw not_implemented("allocator_global_heap::~allocator_global_heap()", "your code should be here...");
+    if (_logger)
+        _logger->trace("allocator_global_heap::~allocator_global_heap - begin");
+
+    if (_logger)
+        _logger->trace("allocator_global_heap::~allocator_global_heap - end");
 }
 
 allocator_global_heap::allocator_global_heap(const allocator_global_heap &other)
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(const allocator_global_heap &other)", "your code should be here...");
+    if (_logger)
+        _logger->trace("allocator_global_heap::copy_ctor - begin");
+
+    _logger = other._logger;
+
+    if (_logger)
+        _logger->trace("allocator_global_heap::copy_ctor - end");
 }
 
 allocator_global_heap &allocator_global_heap::operator=(const allocator_global_heap &other)
 {
-    throw not_implemented("allocator_global_heap &allocator_global_heap::operator=(const allocator_global_heap &other)", "your code should be here...");
+    if (_logger)
+        _logger->trace("allocator_global_heap::copy_assign - begin");
+
+    if (this != &other)
+        _logger = other._logger;
+
+    if (_logger)
+        _logger->trace("allocator_global_heap::copy_assign - end");
+
+    return *this;
 }
 
 bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) const noexcept
 {
-    throw not_implemented("bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) const noexcept", "your code should be here...");
+    if (_logger)
+        _logger->trace("allocator_global_heap::do_is_equal - begin");
+
+    bool result = (this == &other);
+
+    if (_logger)
+        _logger->trace("allocator_global_heap::do_is_equal - end");
+
+    return result;
 }
 
 allocator_global_heap::allocator_global_heap(allocator_global_heap &&other) noexcept
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(allocator_global_heap &&) noexcept", "your code should be here...");
+    if (_logger)
+        _logger->trace("allocator_global_heap::move_ctor - begin");
+
+    _logger = other._logger;
+    other._logger = nullptr;
+
+    if (_logger)
+        _logger->trace("allocator_global_heap::move_ctor - end");
 }
 
 allocator_global_heap &allocator_global_heap::operator=(allocator_global_heap &&other) noexcept
 {
-    throw not_implemented("allocator_global_heap &allocator_global_heap::operator=(allocator_global_heap &&) noexcept", "your code should be here...");
+    if (_logger)
+        _logger->trace("allocator_global_heap::move_assign - begin");
+
+    if (this != &other) {
+        _logger = other._logger;
+        other._logger = nullptr;
+    }
+
+    if (_logger)
+        _logger->trace("allocator_global_heap::move_assign - end");
+
+    return *this;
 }
